7bad03db503109c391c326b1f6dd92f3f33ad38c
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2018-04-20 11:55:14 -0300

    Mais alguma funções

--- Programação Funcional/introducao.hs
+++ Programação Funcional/introducao.hs
@@ -39,9 +39,9 @@ fatorial x = x * fatorial (x-1)
 
 isPrime :: Int -> Bool
 isPrime x = do
-	let list = [a| a <- [2..x], x`mod`a == 0]
-	(head list) == (last list)
-	
+ let list = [a| a <- [2..x], x`mod`a == 0]
+ (head list) == (last list)
+
 
 {-
 - Calcula um termo da sequencia de Fibonnacci. Voce pode trabalhar com listas. 
@@ -54,18 +54,27 @@ fib x = fib (x-1) + fib (x-2)
 {-
 - Calcula um MDC de dois numeros usando o algoritmo de Euclides. 
 -}
-mdc x y = undefined
-
+mdc :: Int -> Int -> Int
+mdc x 0 = x
+mdc 0 y = y
+mdc x y
+ |x > y = mdc y (x`mod`y) 
+ |otherwise = mdc x (y`mod`x)
 {-
 - Calcula um MMC de dois numeros. 
 -}
-mmc x y = undefined
+mmc :: Int -> Int -> Int
+mmc 0 y = 0
+mmc x 0 = 0
+mmc x y = div (x * y) (mdc x y)
 
 {-
 - Determina se dois numeros inteiros positivos sao co-primos. Dois numeros sao co-primos se 
 - o mdc deles for igual a 1. Ex: coprimo 35 64 = True 
 -}
-coprimo x y = undefined
+coprimo x y
+ | mdc x y == 1 = True
+ | otherwise = False
 
 {-
 - Calcula a conjectura de Goldbach, que diz que um numero par maior que 2 pode ser escrito como a soma de dois numeros primos. Ex: 28 = 5 + 23.
@@ -75,7 +84,8 @@ goldbach x = undefined
 
 main:: IO()
 main = do
-	a <- getLine
-	let result = isPrime (read a)
-	print result
-	
+ a <- getLine
+ b <- getLine
+ let result = coprimo (read a) (read b)
+ print result
+
